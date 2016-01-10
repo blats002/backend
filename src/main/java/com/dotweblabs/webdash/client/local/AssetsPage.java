231d0b759a572258e863e7f55665fd3a4d71aba9
Kerby Martino <kerby@dotweblabs.com>
2016-01-10 17:28:18 +0800

    upload modal widget

--- src/main/java/com/dotweblabs/webdash/client/local/AssetsPage.java
+++ src/main/java/com/dotweblabs/webdash/client/local/AssetsPage.java
@@ -3,10 +3,14 @@ package com.divroll.webdash.client.local;
 import com.divroll.webdash.client.local.widgets.Footer;
 import com.divroll.webdash.client.local.widgets.Navbar;
 import com.divroll.webdash.client.local.widgets.Sidebar;
+import com.divroll.webdash.client.local.widgets.modals.UploadWebsiteModal;
+import com.divroll.webdash.client.resources.js.Resources;
+import com.google.gwt.core.client.ScriptInjector;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.inject.Inject;
 import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.nav.client.local.PageShown;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 import javax.enterprise.context.Dependent;
@@ -30,6 +34,15 @@ public class AssetsPage extends Composite{
     @Inject
     @DataField
     Button logout;
+
+    @Inject
+    @DataField
+    UploadWebsiteModal uploadAsset;
+
+    @PageShown
+    public void ready(){
+
+    }
 
 }
 
