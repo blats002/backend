eafaa30545bee06600cb05d8e461be53b082f8e0
Hanan <hananquindara@gmail.com>
2016-01-12 11:45:53 +0800

    [CARD - 7] New Blog Page

--- src/main/java/com/dotweblabs/webdash/client/local/BlogPage.java
+++ src/main/java/com/dotweblabs/webdash/client/local/BlogPage.java
@@ -3,13 +3,17 @@ package com.divroll.webdash.client.local;
 import com.divroll.webdash.client.local.widgets.Footer;
 import com.divroll.webdash.client.local.widgets.Navbar;
 import com.divroll.webdash.client.local.widgets.Sidebar;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.inject.Inject;
 import org.boon.Ok;
 import org.eclipse.xtend.lib.annotations.Data;
 import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.nav.client.local.TransitionTo;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 
 import javax.enterprise.context.Dependent;
@@ -29,5 +33,18 @@ public class BlogPage extends Composite{
     @Inject
     @DataField
     Sidebar menu;
+
+    @Inject
+    @DataField
+    Anchor Add;
+
+    @Inject
+    TransitionTo <NewBlogPage> newBlogPage;
+
+    @EventHandler ("Add")
+    public void Add(ClickEvent event){
+        event.preventDefault();
+        newBlogPage.go();
+    }
 
 }
