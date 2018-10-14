66978dd2e21e4a7c967df44bedd400d586fc9f55
@kerbymart <kerbymart@gmail.com>
2018-10-14 11:13:12 -0700

    BACKEND-28 Fix VFS resource

--- src/main/java/com/divroll/backend/resource/FileResource.java
+++ src/main/java/com/divroll/backend/resource/FileResource.java
@@ -2,9 +2,15 @@ package com.divroll.backend.resource;
 
 import com.divroll.backend.model.File;
 import org.restlet.representation.Representation;
+import org.restlet.resource.Delete;
+import org.restlet.resource.Get;
+import org.restlet.resource.Post;
 
 public interface FileResource {
+    @Post
     File createFile(Representation entity);
+    @Delete
     void deleteFile(Representation entity);
+    @Get
     Representation getFile(Representation entity);
 }
