785490bc538c47a326aeeddfbc516fe43074f840
Kerby Martino <kerby@divroll.com>
2020-03-14 02:56:29 -0700

    Copyright fix

--- src/test/java/com/divroll/backend/TestServlet.java
+++ src/test/java/com/divroll/backend/TestServlet.java
@@ -1,17 +1,29 @@
 /*
-*
-* Copyright (c) 2017 Kerby Martino and Divroll. All Rights Reserved.
-* Licensed under Divroll Commercial License, Version 1.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*   https://www.divroll.com/licenses/LICENSE-1.0
-*
-* Unless required by applicable law or agreed to in writing, software distributed
-* under the License is distributed as Proprietary and Confidential to
-* Divroll and must not be redistributed in any form.
-*
-*/
+ * Divroll, Platform for Hosting Static Sites
+ * Copyright (C) 2019  Kerby Martino
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Other licenses:
+ * -----------------------------------------------------------------------------
+ * Commercial licenses for this work are available. These replace the above
+ * AGPL 3.0 and offer limited warranties, support, maintenance, and commercial
+ * deployments.
+ *
+ * For more information, please email: support@divroll.com
+ *
+ */
 package com.divroll.backend;
 
 import javax.servlet.ServletException;
--- src/test/java/com/divroll/backend/certificates/PrivateKeyValidatorTest.java
+++ src/test/java/com/divroll/backend/certificates/PrivateKeyValidatorTest.java
@@ -1,3 +1,30 @@
+/*
+ * Divroll, Platform for Hosting Static Sites
+ * Copyright (C) 2019  Kerby Martino
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Other licenses:
+ * -----------------------------------------------------------------------------
+ * Commercial licenses for this work are available. These replace the above
+ * AGPL 3.0 and offer limited warranties, support, maintenance, and commercial
+ * deployments.
+ *
+ * For more information, please email: support@divroll.com
+ *
+ */
+
 package com.divroll.backend.certificates;
 
 import org.junit.After;
